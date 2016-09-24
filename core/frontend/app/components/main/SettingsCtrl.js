8fab86d4146fd86a9054fe6a41566fc39c1db734
TilmannBach <tilli14@msn.com>
2016-09-24 20:58:20 +0200

    implemented settings interface #5

--- core/frontend/app/components/main/SettingsCtrl.js
+++ core/frontend/app/components/main/SettingsCtrl.js
@@ -9,7 +9,15 @@
         vm.open = $stateParams.tab || 'main';
         vm.openSettings = goto => { $state.go('settings', { tab: goto }); };
 
-        $http.get('/api/v1/modules/list')
+        vm.saveSetting = saveSetting;
+
+        function saveSetting(moduleId, setting, value, form) {
+            $http.put(['/api/v1/storage/module', moduleId, setting].join('/'), JSON.stringify(value)).then(result => {
+                form.$setPristine();
+            });
+        }
+
+        $http.get('/api/v1/modules/')
             .then(result => {
                 vm.modules = result.data;
             });
